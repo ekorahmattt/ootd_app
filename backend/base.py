0d2e1e0d3af2a2e0ddb6f4222d31d17538774675
Fahmi Adrian <adrian.fahmi23@gmail.com>
2025-01-25 15:35:51 +0800

    copyng ootd diffusion run file

--- backend/base.py
+++ backend/base.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS # type: ignore
 from PIL import Image, ImageOps
+import subprocess
 import os
 
 api = Flask(__name__)
@@ -20,6 +21,10 @@ def process():
         image = Image.open(model)
         cropped_img = ImageOps.fit(image, (768, 1024), centering=(0.5,0.5))
         cropped_img.save("../public"+MODEL_PATH)
+
+        # script_path = os.path.join(os.getcwd(), 'run', 'run_ootd.py')
+        # command = ['python', script_path,
+        #            '--model_path', ]
 
         return jsonify({
             "message" : "Berhasil Disimpan",
